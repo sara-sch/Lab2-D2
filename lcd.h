05d8a83212e4a1e956eafa8072f52c44ce4c3623
sara-sch <99369223+sara-sch@users.noreply.github.com>
2022-08-02 23:14:07 -0600

    Laboratorio 2

---
+++ lcd.h
@@ -0,0 +1,104 @@
+/* 
+ * File: LCD.h  
+ * Se utilizó y se adaptaron las librerías de Ligo George 
+ * de la página www.electrosome.com
+ * Enlace: https://electrosome.com/lcd-pic-mplab-xc8/
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef LCD_H
+#define	LCD_H
+
+#ifndef _XTAL_FREQ
+#define _XTAL_FREQ 4000000
+#endif
+
+#ifndef RS
+#define RS RE1
+#endif
+
+#ifndef EN
+#define EN RE2
+#endif
+
+#ifndef D0
+#define D0 RD0
+#endif
+
+#ifndef D1
+#define D1 RD1
+#endif
+
+#ifndef D2
+#define D2 RD2
+#endif
+
+#ifndef D3
+#define D3 RD3
+#endif
+
+#ifndef D4
+#define D4 RD4
+#endif
+
+#ifndef D5
+#define D5 RD5
+#endif
+
+#ifndef D6
+#define D6 RD6
+#endif
+
+#ifndef D7
+#define D7 RD7
+#endif
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+//LCD Functions Developed by electroSome
+
+//4bits
+
+void Lcd_Port(char a);
+
+void Lcd_Cmd(char a);
+
+void Lcd_Clear(void);
+
+void Lcd_Set_Cursor(char a, char b);
+
+void Lcd_Init(void);
+
+void Lcd_Write_Char(char a);
+
+void Lcd_Write_String(char *a);
+
+void Lcd_Shift_Right(void);
+
+void Lcd_Shift_Left(void);
+
+// 8bits
+
+void Lcd_Cmd_8(char a);
+
+void Lcd_Port_8(char a);
+
+void Lcd_Init_8(void);
+
+void Lcd_Clear_8(void);
+
+void Lcd_Set_Cursor_8(char a, char b);
+
+void Lcd_Write_Char_8(char a);
+
+void Lcd_Shift_Right_8(void);
+
+void Lcd_Shift_Left_8(void);
+
+void Lcd_Write_String_8(char *a);
+
+
+#endif	/* LCD_H */
+
